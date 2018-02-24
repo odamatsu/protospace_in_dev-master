1bdac15ae84e253dd25f1d33ab23b990e2e1cd0f
carshow <saladvar1212@gmail.com>
2018-02-24 15:47:00 +0900

    remove_conflict

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -23,7 +23,10 @@ class PrototypesController < ApplicationController
   def show
   end
 
-  def edit
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy if prototype.user_id == current_user.id
+    redirect_to root_path
   end
 
   private
