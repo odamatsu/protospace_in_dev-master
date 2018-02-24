5b99025d1e26dfc139822a04793968b4957d6d37
carshow <saladvar1212@gmail.com>
2018-02-24 14:15:08 +0900

    Remove conflict

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -15,8 +15,6 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to :root, notice: 'New prototype was successfully created'
     else
-      flash.now[:alert] = 'YNew prototype was unsuccessfully created'
-      render action: :new
      end
   end
 
