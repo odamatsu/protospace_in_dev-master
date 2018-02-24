afb01a2f61d4e533afa1f8a3df51b4269dfbc3c0
carshow <saladvar1212@gmail.com>
2018-02-24 14:17:47 +0900

    Edit spelling_miss

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -15,8 +15,8 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to :root, notice: 'New prototype was successfully created'
     else
-      flash.now[:alert] = 'YNew prototype was unsuccessfully created'
-      render action: :new
+      flash.now[:alert] = 'New prototype was unsuccessfully created'
+      render :new
      end
   end
 
