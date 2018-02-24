d09efc5744c6e738b8bf1fa794db9f61274d7065
carshow <saladvar1212@gmail.com>
2018-02-24 13:43:13 +0900

    Change from "redirect_to" to "render"

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -15,7 +15,8 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to :root, notice: 'New prototype was successfully created'
     else
-      redirect_to ({ action: new }), alert: 'YNew prototype was unsuccessfully created'
+      flash.now[:alert] = 'New prototype was unsuccessfully created'
+      render :new
      end
   end
 
