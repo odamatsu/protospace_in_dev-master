f921e4b1c1af1a627ff9b8e8e4bde553b50fb275
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-03 16:08:02 +0900

    implement new function to delete action

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -2,7 +2,7 @@ $(function (){
 
     function buildHTML(data) {
       var html = `
-        <ul class="dropdown-menu">
+        <ul class="dropdown_menu_list">
           <li>
             <a rel="nofollow" data-method="delete" href="/prototypes/${data}">Delete
             </a>
@@ -19,7 +19,19 @@ $(function (){
   $("#dropdownMenu").on("click", function(){
     var prototype_id = $(this).attr("value");
     var html = buildHTML(prototype_id);
-    $("#actionMenu").append(html);
+    var nextClass = $(this).next().prop("class");
+    if (nextClass === "dropdown_menu_list"){
+      $(".dropdown_menu_list").remove();
+    } else {
+      $("#actionMenu").append(html);
+    }
   });
+
+  // $("body").on("click", function(e){
+  //     var clickedElement = $(e.target);
+  //     if($(clickedElement).hasClass("body")){
+  //         $("body").fadeOut();
+  //     }
+  // });
 
 });
