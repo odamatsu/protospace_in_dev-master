906b383cbe0713717457a37883d4658885973a3c
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-03 14:57:34 +0900

    implement modal window for JavaScript  in mypage

---
+++ app/assets/javascripts/mypage.js
@@ -0,0 +1,25 @@
+$(function (){
+
+    function buildHTML(data) {
+      var html = `
+        <ul class="dorpdown-menu">
+          <li>
+            <a rel="nofollow" data-method="delete" href="/prototypes/${data}">Delete
+            </a>
+          </li>
+          <li>
+            <a href="/">Edit
+            </a>
+          </li>
+        </ul>
+      `
+      return html;
+    }
+
+  $("#dropdownMenu").on("click", function(){
+    var prototype_id = $(this).attr("value");
+    var html = buildHTML(prototype_id);
+    $("#actionMenu").append(html);
+  });
+
+});
