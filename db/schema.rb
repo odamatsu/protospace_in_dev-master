5d72b4860f7b163abf903c63f3a38c617f3f7ce7
carshow <saladvar1212@gmail.com>
2018-03-03 18:11:40 +0900

    Implement migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303082158) do
+ActiveRecord::Schema.define(version: 20180303090848) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 20180303082158) do
   add_index "captured_images", ["prototype_id"], name: "index_captured_images_on_prototype_id", using: :btree
 
   create_table "comments", force: :cascade do |t|
-    t.text     "text",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.text     "text",         limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.integer  "prototype_id", limit: 4
   end
 
   create_table "prototypes", force: :cascade do |t|
