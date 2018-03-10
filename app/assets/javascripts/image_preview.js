f29cab738a398e9284d714edf94c1bd73ce54d10
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-03-10 15:55:00 +0900

    modify image uploade

--- app/assets/javascripts/image_preview.js
+++ app/assets/javascripts/image_preview.js
@@ -1,35 +1,37 @@
 $(function(){
-  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
   $('form').on('change', 'input[type="file"]', function(e) {
-    if($(".preview")){
-      $(".preview").remove();
+    if($(e.target).siblings("img")){
+      $(e.target).siblings("img").remove();
     }
     var file = e.target.files[0];
     var reader = new FileReader();
-    var $preview = $("#main_image_uploader");
+    var $preview = $(e.target);
     var t = this;
-    // 画像ファイル以外の場合は何もしない
     if(file.type.indexOf("image") < 0){
       return false;
     }
 
-    // ファイル読み込みが完了した際のイベント登録
     reader.onload = (function(file) {
       return function(e) {
-        //既存のプレビューを削除
-        // $preview.empty();
-        // .prevewの領域の中にロードした画像を表示するimageタグを追加
-        $preview.append($('<img>').attr({
-                  src: e.target.result,
-                  width:"730px",
-                  height: "500px",
-                  class: "preview",
-                  title: file.name
-              }));
+        if ($preview.attr("id").match(/0/)!==null){
+          $preview.after($('<img>').attr({
+                    src: e.target.result,
+                    width:"730px",
+                    height: "500px",
+                    class: "preview",
+                    title: file.name
+                }));
+        } else {
+          $preview.after($('<img>').attr({
+                    src: e.target.result,
+                    width:"230px",
+                    height: "200px",
+                    class: "preview",
+                    title: file.name
+                }));
+        }
       };
     })(file);
-    // debugger
-    // $("#prototype_captured_images_attributes_0_content").prop("disabled", false);
     reader.readAsDataURL(file);
   });
 
