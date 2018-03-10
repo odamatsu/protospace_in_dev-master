a05f4a9b97ed9e2240665d1effebb087678fa9c7
carshow <saladvar1212@gmail.com>
2018-03-10 12:11:34 +0900

    add_column, :comments, ;user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303090848) do
+ActiveRecord::Schema.define(version: 20180310030710) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180303090848) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.integer  "prototype_id", limit: 4
+    t.integer  "user_id",      limit: 4
   end
 
   create_table "prototypes", force: :cascade do |t|
