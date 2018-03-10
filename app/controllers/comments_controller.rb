3a50e58538b8f35822112e52552a8c5e58a1302a
Hiro Tech <odamatsu@gmail.com>
2018-03-10 12:50:23 +0900

    add controller action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,24 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
-  def create
 
+  def create
+    @comment = Comment.create(text: comment_params[:text], comment_id: comment_params[:comment_id], user_id: current_user.id)
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def create.params
+    params.permit(:text, :comment_id)
   end
 end
