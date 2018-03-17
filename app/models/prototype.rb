47067b53c0f00f444fc033335ec96d77846b6c4b
carshow <saladvar1212@gmail.com>
2018-03-17 14:06:58 +0900

    Association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,7 @@ class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images, dependent: :destroy
   has_many :comments
+  has_many :likes
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
 
@@ -20,5 +21,9 @@ class Prototype < ActiveRecord::Base
 
   def posted_date
     created_at.strftime('%b %d %a')
+  end
+
+  def like_user(user)
+   likes.find_by(user_id: user)
   end
 end
