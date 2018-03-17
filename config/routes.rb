1980ca541466b19cbaabb3cc819fdeb92647b428
carshow <saladvar1212@gmail.com>
2018-03-17 14:05:32 +0900

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-
   resources :prototypes do
+    resources :likes, only: [:create, :destroy]
   	resources :comments, only: [:create, :edit, :destroy, :update]
   end
   resources :users, only: [:show, :edit, :update]
