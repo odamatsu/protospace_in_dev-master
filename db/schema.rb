67b3a1088cc16f493ec1ff1166aa888f752e8efa
carshow <saladvar1212@gmail.com>
2018-03-17 17:17:53 +0900

    Resolve conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317030948) do
+ActiveRecord::Schema.define(version: 20180317031902) do
 
   create_table "captured_images", force: :cascade do |t|
     t.string  "content",      limit: 255
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20180317030948) do
   add_index "likes", ["prototype_id"], name: "fk_rails_8847d87628", using: :btree
   add_index "likes", ["user_id"], name: "fk_rails_1e09b5dabf", using: :btree
 
+  create_table "prototype_tags", force: :cascade do |t|
+    t.integer  "prototype_id", limit: 4
+    t.integer  "tag_id",       limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "prototype_tags", ["tag_id", "prototype_id"], name: "index_prototype_tags_on_tag_id_and_prototype_id", using: :btree
+  add_index "prototype_tags", ["tag_id"], name: "index_prototype_tags_on_tag_id", using: :btree
+
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.string   "catch_copy",  limit: 255
@@ -50,6 +60,14 @@ ActiveRecord::Schema.define(version: 20180317030948) do
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
