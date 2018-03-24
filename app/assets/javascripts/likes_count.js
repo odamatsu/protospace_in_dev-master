c18278574a4badfd43f3e621f46582e83ce8daaf
carshow <saladvar1212@gmail.com>
2018-03-24 14:11:25 +0900

    Create likes_count.js

---
+++ app/assets/javascripts/likes_count.js
@@ -0,0 +1,41 @@
+$(function(){
+  var $heart= $("#like-button img");
+  $("#like-button").on("click", function(){
+    var id = $(".proto-page").attr("data-id");
+    var $likes_sum = $("#like-button span");
+    var $sum = $likes_sum.attr("data-count");
+    var $sum = Number( $sum );
+    like_function(id,$(this),$heart,$likes_sum,$sum);
+  });
+
+  function like_function(id,button,heart,sum, count){
+    if( button.hasClass("like") ){
+      // $.ajax({
+      //   url: "/prototypes/" + id + "/likes",
+      //   type: "delete",
+      //   dataType: "json",
+      //   processData: false,
+      //   contentType: false
+      // })
+      // .done(function(data){
+        button.removeClass("like").addClass('dislike');
+        heart.attr("src", "/assets/icon_heart-20100b1c862f59bb23666746a16cdbd86d418436888ef57ed262f5baa649a69c.svg");
+        sum.html( count);
+      // })
+    } else {
+      // $.ajax({
+      //   url: "/prototypes/" + id + "/likes",
+      //   type: "post",
+      //   dataType: "json",
+      //   processData: false,
+      //   contentType: false
+      // })
+      // .done(function(data){
+        button.removeClass("dislike").addClass('like');
+        heart.attr("src", "/assets/icon_heart_red-65035f3e2f106cde180f2c1448bce917839fc297e339fcbcd65cf41eb92e8ec1.svg");
+        sum.html(count + 1);
+      // })
+    }
+  }
+
+});
